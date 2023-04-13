735e2d423391276c159a7c9231f752c981edb827
J tuluve <jtuluve@gmail.com>
2023-04-13 20:13:16 +0530

    clear name function

--- js/index.js
+++ js/index.js
@@ -1,26 +1,42 @@
-function sleep(milliseconds) {
-    const date = Date.now();
-    let currentDate = null;
-    do {
-      currentDate = Date.now();
-    } while (currentDate - date < milliseconds);
+/*function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
+  async function trial2() {
+    temptext = text + alph[j];
+    textvar.innerHTML = temptext;
+    if (alph[j] == jname[i]) {
+      text = temptext;
+      return;
+    }
+    j++;
+    if (j < alph.length) {
+      await sleep(50)
+      trial2()
+      //sleep(50).then(function(){trial2()})
+
+  }
+}
+function namee(){
     let textvar = document.getElementById("myname");
-    let i =0;
-    let j= 0;
+    let i = 8;
+    let j = 0;
     let text = textvar.innerHTML;
     let temptext = text;
     let jname = "Hi. I'm Jnanesh."
-    let alph = [".","A","B","C","D","E","F","G","H","I","J","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s"]
-    for(i=8;i<jname.length;i++){
-        for(j = 0; j<alph.length;j++){
-            temptext= text + alph[j];
-            textvar.innerHTML = text;
-            if(alph[j]==jname[i]){
-                text = temptext;
-                break;
-            }
-            sleep(500)
-            console.log(temptext)
+    let alph = [".", "A","B","C","D","E","F","G","H","I","J","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s"]
+    trial();
+    async function trial(){ 
+        await sleep(500)
+        await trial2();
+        
+        if(alph[j]==jname[i]){
+        i++;
+        j = 0;
+        }
+        if (i < jname.length) {
+            trial()
         }
     }
+  }
+  namee();
+  */
